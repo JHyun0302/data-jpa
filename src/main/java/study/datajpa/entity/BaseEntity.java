144433496b49e8c9a1a4e33349a53ee49ee29b25
jaehyun <wogustkfkd11@gmail.com>
2023-03-01 19:54:09 +0900

    등록자 수정자 정보가 필요한 테이블에만 넣어주기

---
+++ src/main/java/study/datajpa/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * 등록자 수정자 정보가 필요한 테이블에만 넣어주기
+ */
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@MappedSuperclass
+public class BaseEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy; //등록한 사람
+
+    @LastModifiedBy
+    private String lastModifiedBy; //수정한 사람
+}
