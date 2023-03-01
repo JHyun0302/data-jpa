b9a3bf125c76abddb4af97450b41c2c787f4b123
jaehyun <wogustkfkd11@gmail.com>
2023-03-01 19:53:54 +0900

    Auditing - 등록/수정한 사람 UUID로 저장 (실무 - 세션의 id)

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,11 +2,22 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing//(modifyOnCreate = false) : update = null (비권장!) //등록일/수정일 같은 공통 관심사
 @SpringBootApplication
 public class DataJpaApplication {
     public static void main(String[] args) {
         SpringApplication.run(DataJpaApplication.class, args);
     }
 
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
 }
