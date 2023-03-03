18c7ed8a33598a5c0562fd9f145b56a50cd5b95b
jaehyun <wogustkfkd11@gmail.com>
2023-03-03 12:12:33 +0900

    명세(Specification): JPA Criteria 사용 - 실무 비권장

---
+++ src/main/java/study/datajpa/repository/MemberSpec.java
@@ -0,0 +1,32 @@
+package study.datajpa.repository;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
+
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+
+/**
+ * 명세: JPA Criteria
+ */
+public class MemberSpec {
+    //팀 이름이 검색 조건
+    public static Specification<Member> teamName(final String teamName) {
+        return (Specification<Member>) (root, query, builder) -> {
+            if (StringUtils.isEmpty(teamName)) {
+                return null;
+            }
+
+            Join<Member, Team> t = root.join("team", JoinType.INNER); //회원과 팀 조인
+            return builder.equal(t.get("name"), teamName); //where 문
+        };
+    }
+
+    public static Specification<Member> username(final String username) {
+        return (Specification<Member>) (root, query, builder) ->
+                builder.equal(root.get("username"), username);
+    }
+}
+
