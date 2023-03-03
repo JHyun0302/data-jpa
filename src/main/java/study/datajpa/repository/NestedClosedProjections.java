aa09584c084c381de8f11700e1f3d94162eed492
jaehyun <wogustkfkd11@gmail.com>
2023-03-03 12:13:29 +0900

    중첩 구조 - 프로젝션

---
+++ src/main/java/study/datajpa/repository/NestedClosedProjections.java
@@ -0,0 +1,15 @@
+package study.datajpa.repository;
+
+/**
+ * 중첩 구조 처리
+ */
+public interface NestedClosedProjections {
+    String getUsername();
+
+
+    TeamInfo getTeam();
+
+    interface TeamInfo {
+        String getName();
+    }
+}
