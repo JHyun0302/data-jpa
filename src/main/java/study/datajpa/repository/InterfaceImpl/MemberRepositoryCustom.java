9ada534c049144cd0cc7cdc8d50322654fcb70e8
JHyun0302 <wogustkfkd11@gmail.com>
2023-03-25 12:34:03 +0900

    spring data jpa_custom

--- src/main/java/study/datajpa/repository/InterfaceImpl/MemberRepositoryCustom.java
+++ src/main/java/study/datajpa/repository/InterfaceImpl/MemberRepositoryCustom.java
@@ -1,4 +1,4 @@
-package study.datajpa.repository;
+package study.datajpa.repository.InterfaceImpl;
 
 import study.datajpa.entity.Member;
 
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * 사용자 정의 인터페이스
  * - 간단한 내용은 스프링 data JPA 쓰고
- * - 복잡한 내용은 Custom해서 JPQL 사용
+ * - 복잡한 내용은 Custom해서 QueryDsl 쓰기
  */
 public interface MemberRepositoryCustom {
     List<Member> findMemberCustom();
