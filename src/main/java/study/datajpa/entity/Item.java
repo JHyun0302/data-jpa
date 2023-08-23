623b244ce0fc1d5e6940a6c365b6dec90134c13e
JHyun0302 <wogustkfkd11@gmail.com>
2023-08-23 23:05:44 +0900

    :bulb: [Comment] @GeneratedValue 못 쓸 때는 Persistable 구현하기

--- src/main/java/study/datajpa/entity/Item.java
+++ src/main/java/study/datajpa/entity/Item.java
@@ -14,8 +14,11 @@ import java.time.LocalDateTime;
 /**
  * 새로운 엔티티 구별하는 방법
  * - 문제: @GeneratedValue 안 쓰고 @Id 할당하는 경우 값을 넘겨주므로 null or 0 이 아니게 됨!
- * -> "새로운 엔티티가 아니라고 판단!!!" => em.merge()!!
+ * -> "새로운 엔티티가 아니라고 판단!!!" => em.persist() X -> em.merge() 작동!!
  * - sol: persistable 인터페이스 구현해서 판단 로직 변경 가능!
+ * <p>
+ * => @GeneratedValue를 쓰지 못하는 상황에서 쓰기!
+ * ex) 테이블 안에 데이터가 너무 많을 경우
  */
 @Entity
 @EntityListeners(AuditingEntityListener.class)
