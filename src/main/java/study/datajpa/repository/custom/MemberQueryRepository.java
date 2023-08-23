498be615dd15abf182cca957f1ee7318787bf7c3
JHyun0302 <wogustkfkd11@gmail.com>
2023-08-23 23:07:27 +0900

    :bulb: [Comment] 복잡한 쿼리는 클래스 분리해서 Querydsl or JPQL로 해결하기

--- src/main/java/study/datajpa/repository/custom/MemberQueryRepository.java
+++ src/main/java/study/datajpa/repository/custom/MemberQueryRepository.java
@@ -1,4 +1,4 @@
-package study.datajpa.repository.InterfaceImpl;
+package study.datajpa.repository.custom;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -10,8 +10,9 @@ import java.util.List;
 /**
  * Custom 기능에 다 몰아넣지 말고
  * - 핵심 biz 기능과 아닌 것 분리
- * - life cycle에 따라 뭘 변경
- * 고려해서 repository 쪼개기
+ * - life cycle에 따라 repository 쪼개기
+ * - ex) 화면 수정하는데 필요한 복잡한 동적 쿼리 = MemberQueryRepository (class 형태로 생성해서 직접 JPQL 쓰기)
+ * - ex) 간단한 조회 or 핵심 biz 기능 쿼리 = MemberRepository (Spring Data JPA) + MemberRepositoryImpl (Querydsl)
  */
 @Repository
 @RequiredArgsConstructor
