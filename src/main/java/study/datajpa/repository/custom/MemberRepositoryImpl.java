2894668e3d44c232b284858a565936e2aa663cf1
JHyun0302 <wogustkfkd11@gmail.com>
2023-08-23 23:11:21 +0900

    :bulb: [Comment] 간단한 CRUD : Spring Data JPA, 복잡한 쿼리 : Querydsl

--- src/main/java/study/datajpa/repository/custom/MemberRepositoryImpl.java
+++ src/main/java/study/datajpa/repository/custom/MemberRepositoryImpl.java
@@ -1,4 +1,4 @@
-package study.datajpa.repository.InterfaceImpl;
+package study.datajpa.repository.custom;
 
 import lombok.RequiredArgsConstructor;
 import study.datajpa.entity.Member;
@@ -6,6 +6,10 @@ import study.datajpa.entity.Member;
 import javax.persistence.EntityManager;
 import java.util.List;
 
+/**
+ * 규칙 : Spring Data JPA 쓰는 인터페이스 이름 + Impl
+ * ex) MemberRepository + Impl
+ */
 @RequiredArgsConstructor
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
     private final EntityManager em;
