f61d76ca8e7460a50aac8c9d85cfbeee18051628
JHyun0302 <wogustkfkd11@gmail.com>
2023-08-24 10:11:53 +0900

    :bulb: [Comment] Projections : @Value 속성

--- src/main/java/study/datajpa/repository/projections/UsernameOnly.java
+++ src/main/java/study/datajpa/repository/projections/UsernameOnly.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 
 public interface UsernameOnly {
     /**
-     * Projections ex) username만으로 조회
+     * Projections ex) username만 조회
      *
      * @Value 있으면: Open Projections
-     * - 단점: DB에서 엔티티 필드 다 조회해온 다음에 계산 -> JPQL Select절 최적화 안됨
+     * - 단점: DB에서 엔티티 정보 다 조회해온 다음에 계산 -> JPQL Select절 최적화 안됨
      * @Value 없으면: Close Projections
+     * - 정확하게 매칭. DB에서 원하는 값(username)만 select절에 담아서 가져옴.
      */
     @Value("#{target.username + ' ' + target.age + ' ' + target.team.name}")
     String getUsername();
